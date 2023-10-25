44984759682541020e2207946129e18ec083fc11
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2023-10-25 03:39:11 -0300

    Replicando/repasando clase 6 de LAB IV. 1° clase de angular (18/09/23) - Laboratorio IV - UTN
    Temas vistos:
    - Concepto de modulo y componente
    - Routing
    - Lazy Loading
    - Scaffolding
    - OnInit
    - OneDataBinding/interpolacion: {{ }}
      (solo lo usamos, no se explico el nombre ni la definicion aun)
    - ngIf()

---
+++ laboratorio-IV/replicando-clases/clase-6/clase6Angular/src/app/modules/home/pages/home-page/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent implements OnInit {
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
